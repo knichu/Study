a2b6afa6a1dab86c4aa4407d59307ed211fc5593
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-14 16:16:47 +0900

    Update 큰수의 법칙.py

--- 이코테 문제풀이/그리디/큰수의 법칙.py
+++ 이코테 문제풀이/그리디/큰수의 법칙.py
@@ -1,9 +1,5 @@
 # 내가 작성한 코드
 
-import time
-start = time.time()
-
-
 n, m, k = map(int, input().split())
 
 c = list(map(int, input().split()))
@@ -17,8 +13,29 @@ sum = ((c[0] * k) + c[1])*a + c[0]*b
 print(sum)
 
 
-end = time.time()
-print(f"{end - start:.5f} sec")
 
-#
+# 답안 예시
 
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first
+result += (m - count) * second
+
+print(result)
+
+
+
+# 코멘트
+
+sort(reverse = True) 해서 앞에 두개를 따오기 보단 sort() 해서 맨뒤 2개를 따오자.  
+속도에서 유의미한 차이가 난다.  
