6fb0017aaa7d6f9ff4fc32153b4aa7ed51f8d43e
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-16 01:25:25 +0900

    Update 시각.py

--- 이코테 문제풀이/구현/시각.py
+++ 이코테 문제풀이/구현/시각.py
@@ -1,1 +1,44 @@
+# 내가 작성한 코드
 
+n = int(input())
+
+count = 0
+
+for i in range(24):
+    for j in range(60):
+        for k in range(60):
+            if n >= 10:
+                if i == n or j == n or k == n:
+                    count += 1
+                else:
+                    continue
+                
+            else:
+                c = list(map(int, str(i) + str(j) + str(k)))
+                if c.count(n) >= 1:
+                    count += 1
+                
+                
+print(count)
+
+
+
+# 답안 예시
+
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
+
+
+
+# 코멘트
+
+문제를 잘못이해했거나 문제를 잘못낸듯....말이 너무 애매함
