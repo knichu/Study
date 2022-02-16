bc6080bf3d7f3d3b93fd57609ad1514cc38236c4
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-16 22:44:06 +0900

    Update 게임 개발.py

--- 이코테 문제풀이/구현/게임 개발.py
+++ 이코테 문제풀이/구현/게임 개발.py
@@ -1,1 +1,12 @@
+n, m = map(int, input().split()) # n - A, m - B
+
+x, y, d = map(int, input().split())
+
+map = [list(map(int, input().split())) for i in range(n)]
+
+w = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+while True:
+    for (i, j) in w:
+        if w[]
 
