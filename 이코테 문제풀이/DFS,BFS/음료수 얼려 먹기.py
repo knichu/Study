95f13e12db301d457063929e98f8b646cfd6e5f9
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-20 11:59:24 +0900

    Update 음료수 얼려 먹기.py

--- 이코테 문제풀이/DFS,BFS/음료수 얼려 먹기.py
+++ 이코테 문제풀이/DFS,BFS/음료수 얼려 먹기.py
@@ -1,1 +1,35 @@
-#
+# 내가 작성한 코드(답안 예시를 봐버림..)
+
+n, m  = map(int, input().splt())
+
+ice_maker = [list(map(int, input().split())) for i in range(n)]
+
+def dfs(x, y):
+    
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+    
+    if ice_maker[x][y] == 0:
+        ice_maker[x][y] = 1
+        
+        dfs(x + 1, y)
+        dfs(x - 1, y)
+        dfs(x, y + 1)
+        dfs(x, y - 1)
+        return True
+    return False
+
+count = 0
+
+for i in range(n):
+    for j in range(m):
+        dfs(i, j) == True:
+            count += 1
+            
+print(count)
+
+
+
+# 코멘트
+
+문제해결위한 아이디어가 잘 생각이 안났다.
