af5ee687628e51d55579b4bf6e65a34bd443ac79
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-24 01:56:59 +0900

    Create 부품찾기.py

---
+++ 이코테 문제풀이/챕터2(실전문제)/이진 탐색/부품찾기.py
@@ -0,0 +1,69 @@
+# 내가 작성한 코드
+
+def i_zin(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1 
+    return None
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+m = int(input())
+b = list(map(int, input().split()))
+
+for i in b:
+    if i_zin(a, i, 0, n - 1) != None:
+        print("Yes", end = ' ')
+    else:
+        print("No", end = ' ')
+        
+ 
+
+# 답안 예시
+
+# 이진 탐색 소스코드 구현 (반복문)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 공백을 기준으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백을 기준으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    result = binary_search(array, i, 0, n - 1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+        
+
+               
+# 코멘트
+
+집합형으로 해결하는건 처음봤다. 그래서 집합에 대해 구글링해서 공부했다.
+        
