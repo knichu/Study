cc2c5373bf5fd6fc99f1b8fa4873b19112638879
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-02-26 13:49:08 +0900

    Update 1로 만들기.py

--- 이코테 문제풀이/챕터2(실전문제)/다이나믹 프로그래밍(DP)/1로 만들기.py
+++ 이코테 문제풀이/챕터2(실전문제)/다이나믹 프로그래밍(DP)/1로 만들기.py
@@ -50,6 +50,27 @@ for i in range(2, x + 1):
 print(d[x])
 
 
-# 코멘트
 
+
+# 코멘트
 앞으로 가능하면 무조껀 보텀업으로 하자. 훨 간단해 보임.
+# 보텀업으로 연습
+
+x = int(input())
+
+d = [0] * 30001
+
+for i in range(2, x + 1):
+  d[i] = d[i - 1] + 1  
+  if i % 2 == 0:
+    d[i] = min(d[i // 2], d[i - 1]) + 1
+  if i % 3 == 0:
+    d[i] = min(d[i // 3], d[i - 1]) + 1
+  if i % 5 == 0:
+    d[i] = min(d[i // 5], d[i - 1]) + 1
+
+print(d[x])
+# 추가 코멘트
+답지 방식 : d[i] = min(d[i], d[i // 2] + 1)
+내방식 : d[i] = min(d[i // 2], d[i - 1]) + 1
+
