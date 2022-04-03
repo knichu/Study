b059f8650a3329e47a49b21ac3fa2bb969458e91
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-04-03 22:42:33 +0900

    Update 커리큘럼 다른방식.py

--- 이코테 문제풀이/챕터2(실전문제)/그래프 이론/커리큘럼 다른방식.py
+++ 이코테 문제풀이/챕터2(실전문제)/그래프 이론/커리큘럼 다른방식.py
@@ -1,8 +1,4 @@
-from collections import deque
-
 n = int(input())  # n = 5
-
-indegree = [0] * (n + 1)
 
 graph = [[] for i in range(n + 1)]
 
@@ -13,19 +9,34 @@ for i in range(1, n + 1):
     time[i] = k[0]
     l = len(k)
     for j in range(1, l-1):
-        graph[i].append(k[j])
-        indegree[i] += 1      
+        graph[i].append(k[j])  
 
+# graph = [[]
+#          []
+#          [1]
+#          [1]
+#          [1, 3]
+#          [3]
+# ]
 
 # time = [0, 10, 10, 4, 4, 3]
 
 q = deque()
 result_time = 0
+current_time = 0
 
 def dfs(graph, start):
-    q.appendleft(start)
-    result_time += time[start + 1]
-    a = q.popleft()
-    for i in graph[a]:
-        if not graph[i]:
-            continue
+    current_time += time[start + 1] 
+    for i in graph[start]:
+        if not graph[start]:
+            result_time = max(result_time, current_time)
+            current_time = ### 이부분을 이전 단계의  current_time을 고려하도록 해야함
+        else:
+            dfs(graph, i)
+
+    print(result_time)
+    current_time = 0
+    result_time = 0
+
+for i in range(n):
+    dfs(graph, i)
