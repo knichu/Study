804dd38a8a073003b9639ac2e1bd9d8e33ca0f0f
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-04-08 16:09:36 +0900

    Update 모험가 길드.py

--- 이코테 문제풀이/챕터3(유형별기출문제)/그리디 문제/모험가 길드.py
+++ 이코테 문제풀이/챕터3(유형별기출문제)/그리디 문제/모험가 길드.py
@@ -1,1 +1,26 @@
+# 내가 작성한 코드
+
+from collections import deque
+
+n = int(input())
+
+k = list(map(int, input().split()))
+k.sort(reverse = True)
+k = deque(k)
+
+result = 0
+
+while k:
+    a = k[0]
+    if a <= n:
+        for i in range(a):
+            k.popleft()
+        result += 1
+        n = n - a
+    else:
+        break
+    
+print(result)
+
+
 
