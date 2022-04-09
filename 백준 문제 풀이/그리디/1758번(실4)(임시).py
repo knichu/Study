1791ebaa1f0da25ad3da4f2a1c175fe580ca4689
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-04-09 23:29:23 +0900

    Update 1758번(실4)(임시).py

--- 백준 문제 풀이/그리디/1758번(실4)(임시).py
+++ 백준 문제 풀이/그리디/1758번(실4)(임시).py
@@ -2,5 +2,14 @@ n = int(input())
 tip = []
 for i in range(n):
     tip.append(int(input()))
-tip.sort()
-data = [0] * 100001
+tip.sort(reverse = True)
+
+total_tip = 0
+
+for i in range(n):
+    if tip[i] - i < 0:
+        break
+    else:
+        total_tip += tip[i] - i
+
+print(total_tip)
