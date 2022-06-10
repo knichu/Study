a577dc0ac19c24ec769c8473cbc41c81805fb67f
gOdminQ <97740172+gOdminQ@users.noreply.github.com>
2022-06-10 18:29:31 +0900

    Create 9095(실3).py

---
+++ 백준 문제 풀이/DP/9095(실3).py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    dp = [0] * 11
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    n = int(input())
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    print(dp[n])
